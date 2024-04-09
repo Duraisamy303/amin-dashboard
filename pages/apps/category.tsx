ca0f8b1311dc2906359bb85625bb2cdfcb5fa617
Duraisamy <duraisamy.p@sanorac.com>
2024-04-09 22:23:50 +0530

    Merge branch 'design' of https://github.com/Duraisamy303/amin-dashboard

--- pages/apps/category.tsx
+++ pages/apps/category.tsx
@@ -40,6 +40,8 @@ const Contacts = () => {
         }
     }, [categoryData]);
 
+    console.log('state', state.categoryList);
+
     const dispatch = useDispatch();
 
     const themeConfig = useSelector((state: any) => state);
@@ -68,12 +70,158 @@ const Contacts = () => {
     };
 
     const [search, setSearch] = useState<any>('');
+    const [contactList] = useState<any>([
+        {
+            id: 1,
+            path: 'profile-35.png',
+            name: 'Alan Green',
+            role: 'Web Developer',
+            email: 'alan@mail.com',
+            location: 'Boston, USA',
+            phone: '+1 202 555 0197',
+            posts: 25,
+            followers: '5K',
+            following: 500,
+        },
+        {
+            id: 2,
+            path: 'profile-35.png',
+            name: 'Linda Nelson',
+            role: 'Web Designer',
+            email: 'linda@mail.com',
+            location: 'Sydney, Australia',
+            phone: '+1 202 555 0170',
+            posts: 25,
+            followers: '21.5K',
+            following: 350,
+        },
+        {
+            id: 3,
+            path: 'profile-35.png',
+            name: 'Lila Perry',
+            role: 'UX/UI Designer',
+            email: 'lila@mail.com',
+            location: 'Miami, USA',
+            phone: '+1 202 555 0105',
+            posts: 20,
+            followers: '21.5K',
+            following: 350,
+        },
+        {
+            id: 4,
+            path: 'profile-35.png',
+            name: 'Andy King',
+            role: 'Project Lead',
+            email: 'andy@mail.com',
+            location: 'Tokyo, Japan',
+            phone: '+1 202 555 0194',
+            posts: 25,
+            followers: '21.5K',
+            following: 300,
+        },
+        {
+            id: 5,
+            path: 'profile-35.png',
+            name: 'Jesse Cory',
+            role: 'Web Developer',
+            email: 'jesse@mail.com',
+            location: 'Edinburgh, UK',
+            phone: '+1 202 555 0161',
+            posts: 30,
+            followers: '20K',
+            following: 350,
+        },
+        {
+            id: 6,
+            path: 'profile-35.png',
+            name: 'Xavier',
+            role: 'UX/UI Designer',
+            email: 'xavier@mail.com',
+            location: 'New York, USA',
+            phone: '+1 202 555 0155',
+            posts: 25,
+            followers: '21.5K',
+            following: 350,
+        },
+        {
+            id: 7,
+            path: 'profile-35.png',
+            name: 'Susan',
+            role: 'Project Manager',
+            email: 'susan@mail.com',
+            location: 'Miami, USA',
+            phone: '+1 202 555 0118',
+            posts: 40,
+            followers: '21.5K',
+            following: 350,
+        },
+        {
+            id: 8,
+            path: 'profile-35.png',
+            name: 'Raci Lopez',
+            role: 'Web Developer',
+            email: 'traci@mail.com',
+            location: 'Edinburgh, UK',
+            phone: '+1 202 555 0135',
+            posts: 25,
+            followers: '21.5K',
+            following: 350,
+        },
+        {
+            id: 9,
+            path: 'profile-35.png',
+            name: 'Steven Mendoza',
+            role: 'HR',
+            email: 'sokol@verizon.net',
+            location: 'Monrovia, US',
+            phone: '+1 202 555 0100',
+            posts: 40,
+            followers: '21.8K',
+            following: 300,
+        },
+        {
+            id: 10,
+            path: 'profile-35.png',
+            name: 'James Cantrell',
+            role: 'Web Developer',
+            email: 'sravani@comcast.net',
+            location: 'Michigan, US',
+            phone: '+1 202 555 0134',
+            posts: 100,
+            followers: '28K',
+            following: 520,
+        },
+        {
+            id: 11,
+            path: 'profile-35.png',
+            name: 'Reginald Brown',
+            role: 'Web Designer',
+            email: 'drhyde@gmail.com',
+            location: 'Entrimo, Spain',
+            phone: '+1 202 555 0153',
+            posts: 35,
+            followers: '25K',
+            following: 500,
+        },
+        {
+            id: 12,
+            path: 'profile-35.png',
+            name: 'Stacey Smith',
+            role: 'Chief technology officer',
+            email: 'maikelnai@optonline.net',
+            location: 'Lublin, Poland',
+            phone: '+1 202 555 0115',
+            posts: 21,
+            followers: '5K',
+            following: 200,
+        },
+    ]);
 
     const [filteredItems, setFilteredItems] = useState<any>(state.categoryList);
 
     const searchContact = () => {
         setFilteredItems(() => {
-            return state.categoryList.filter((item: any) => {
+            return contactList.filter((item: any) => {
                 return item.name.toLowerCase().includes(search.toLowerCase());
             });
         });
@@ -195,6 +343,8 @@ const Contacts = () => {
                 </div>
             </div>
 
+
+           
             {value === 'list' && (
                 <div className="panel mt-5 overflow-hidden border-0 p-0">
                     <div className="table-responsive">
